8f52cd98881cd8c91db92012dfb92cd2151c5342
pandora-1 <michaelkahari.mk@gmail.com>
2024-05-07 12:33:13 +0700

    fix bug in plotly

--- src/components/history/plotly_chart.tsx
+++ src/components/history/plotly_chart.tsx
@@ -7,11 +7,11 @@ interface GraphProps {
 }
 
 const Plotly: React.FC<GraphProps> = ({ filePath }) => {
-  const [dataPengukuranAsli, setDataPengukuranAsli] = useState<number[]>([]);
-  const [dataBaseline1, setDataBaseline1] = useState<number[]>([]);
-  const [dataBaseline2, setDataBaseline2] = useState<number[]>([]);
-  const [dataPuncak1, setDataPuncak1] = useState<number[]>([]);
-  const [dataPuncak2, setDataPuncak2] = useState<number[]>([]);
+  const [dataPengukuranAsli, setDataPengukuranAsli] = useState<number[][]>([]);
+  const [dataBaseline1, setDataBaseline1] = useState<number[][]>([]);
+  const [dataBaseline2, setDataBaseline2] = useState<number[][]>([]);
+  const [dataPuncak1, setDataPuncak1] = useState<number[][]>([]);
+  const [dataPuncak2, setDataPuncak2] = useState<number[][]>([]);
   const [isCV, setIsCV] = useState<Boolean>(false);
 
   useEffect(() => {
@@ -41,7 +41,7 @@ const Plotly: React.FC<GraphProps> = ({ filePath }) => {
 
             xValues = data.baseline_reduction.map(([x , _]: [number, unknown]) => x); // Extract x values
             yValues = data.baseline_reduction.map(([_, y] : [unknown, number]) => y); // Extract y values
-
+            
             setDataBaseline2([xValues, yValues]);
 
             xValues = [data.info.oxidation.v, data.info.oxidation.v];
@@ -61,14 +61,16 @@ const Plotly: React.FC<GraphProps> = ({ filePath }) => {
     fetchData();
   }, []);
 
+  console.log(dataPengukuranAsli[0])
+
   return (
     <div>
       <Plot
         data={[
           {
             type: 'scatter',
-            x: [dataPengukuranAsli[0]], // Wrap the number in an array
-            y: [dataPengukuranAsli[1]], // Wrap the number in an array
+            x: dataPengukuranAsli[0], // Wrap the number in an array
+            y: dataPengukuranAsli[1], // Wrap the number in an array
             mode: 'lines',
             name: 'Pengukuran asli',
             line: {
@@ -78,8 +80,8 @@ const Plotly: React.FC<GraphProps> = ({ filePath }) => {
           },
           {
             type: 'scatter',
-            x: [dataBaseline1[0]], // Wrap the number in an array
-            y: [dataBaseline1[1]], // Wrap the number in an array
+            x: dataBaseline1[0], // Wrap the number in an array
+            y: dataBaseline1[1], // Wrap the number in an array
             mode: 'lines',
             name: isCV ? "Baseline correction - oksidasi" : "Baseline correction",
             line: {
@@ -89,8 +91,8 @@ const Plotly: React.FC<GraphProps> = ({ filePath }) => {
           },
           isCV ? {
             type: 'scatter',
-            x: [dataBaseline2[0]], // Wrap the number in an array
-            y: [dataBaseline2[1]], // Wrap the number in an array
+            x: dataBaseline2[0], // Wrap the number in an array
+            y: dataBaseline2[1], // Wrap the number in an array
             mode: 'lines',
             name: 'Baseline correction - reduksi',
             line: {
@@ -100,8 +102,8 @@ const Plotly: React.FC<GraphProps> = ({ filePath }) => {
           } : {},
           {
             type: 'scatter',
-            x: [dataPuncak1[0]], // Wrap the number in an array
-            y: [dataPuncak1[1]], // Wrap the number in an array
+            x: dataPuncak1[0], // Wrap the number in an array
+            y: dataPuncak1[1], // Wrap the number in an array
             mode: 'lines',
             name: isCV ? "Puncak - oksidasi" : "Puncak",
             line: {
@@ -112,8 +114,8 @@ const Plotly: React.FC<GraphProps> = ({ filePath }) => {
           },
           isCV ? {
             type: 'scatter',
-            x: [dataPuncak2[0]], // Wrap the number in an array
-            y: [dataPuncak2[1]], // Wrap the number in an array
+            x: dataPuncak2[0], // Wrap the number in an array
+            y: dataPuncak2[1], // Wrap the number in an array
             mode: 'lines',
             name: 'Puncak - reduksi',
             line: {
