6a7cfb997db7b307e92034eb4c51b659fbce4fcb
pandora-1 <michaelkahari.mk@gmail.com>
2024-05-21 18:55:03 +0700

    fix: fixing multiple scroll in analysis

--- src/pages/dashboard/analysis.tsx
+++ src/pages/dashboard/analysis.tsx
@@ -24,11 +24,8 @@ const Analysis = () => {
             <div className="flex overflow-hidden bg-white pt-16">
             <Sidebar clicked="pengujian"/>
             <div className="bg-gray-900 opacity-50 hidden fixed inset-0 z-10 h-screen" id="sidebarBackdrop"></div>
-                <div id="main-content" className="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64 h-screen">
+                <div id="main-content" className="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64 h-screen mb-10">
                     <FileUploadComponent jwt={authProvider.getJwt()!}/>
-                    <p className="fixed text-center text-sm text-gray-500 bottom-10 left-0 right-0">
-                        Made by <a href="https://www.itb.ac.id/" className="text-slate-700 hover:text-slate-900" target="_blank" rel="noopener noreferrer">ITB</a>.
-                    </p>
                 </div>
             </div>
             <Footer />
