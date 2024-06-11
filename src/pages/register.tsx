077dbcaae11eabff29fc18a127541daaf624a2f9
pandora-1 <michaelkahari.mk@gmail.com>
2024-06-11 17:11:31 +0700

    feat: add disabled button

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -51,7 +51,7 @@ const Register = () => {
                 {errors.pohone_number && <p className="text-red-500 text-xs mt-1">Email wajib diisi</p>}
                 <input {...register('password')} className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5" type="password" placeholder="Password" />
                 {errors.password && <p className="text-red-500 text-xs mt-1">Password wajib diisi</p>}
-                <button type="submit" className="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                <button disabled={loading} type="submit" className="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
                   <svg className="w-6 h-6 -ml-2" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                     <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
                     <circle cx="8.5" cy="7" r="4" />
